b7a8b381623323363cbcf45de7da5fca1e295a02
Hugo Pereira <renedx92@gmail.com>
2023-06-23 00:54:22 +0000

    Practica en la clase de Promesas

--- src/es6/06-promises.js
+++ src/es6/06-promises.js
@@ -19,13 +19,31 @@ const promise = (answer) => {
   });
 };
 
-promise(false)
+const fulFillFn = () => {
+  console.log("Correcto");
+};
+
+const rejectFn = () => {
+  console.log("Ups, algo no anduvo");
+};
+
+promise(true)
+  .then((response) => {
+    console.log("primera promesa")
+  })
   .then((response) => {
     console.log(response);
   })
   .catch((err) => {
     console.log(err);
+    console.log('error catch');
   });
+
+promise(false)
+  .then(fulFillFn, rejectFn)
+  //.catch((err) => {
+    //console.log(err);
+  //});
 /* 
 La clase Promise y sus métodos then y catch fueron añadidos en ES6. Esto resuelve un problema del manejo del asincronismo con callbacks, llamado Callback Hell.
 .then() y .catch() son metodos de la clase Promise que retorna a traves de la funcion promise.
