ff2491a36096d3172a73b60b0afb22dd8bbc1e36
Hugo Pereira <renedx92@gmail.com>
2023-06-25 00:13:02 +0000

    Nuevo ejemplo para la clase promises.js y agregado nueva clase sobre Clases ;)

--- src/es6/06-promises.js
+++ src/es6/06-promises.js
@@ -1,5 +1,6 @@
 /*
  * Una promesa es una forma de manejar el asincronismo en JavaScript y se representa como un objeto que puede generar un valor único a futuro, que tiene dos estados, o está resuelta o incluye una razón por la cual no ha sido resuelta la solicitud.
+Una Promise (promesa en castellano) es un objeto que representa la terminación o el fracaso de una operación asíncrona.
  */
 
 // Mi funcion expresiva
@@ -47,3 +48,30 @@ promise(false).then(fulFillFn, rejectFn);
 La clase Promise y sus métodos then y catch fueron añadidos en ES6. Esto resuelve un problema del manejo del asincronismo con callbacks, llamado Callback Hell.
 .then() y .catch() son metodos de la clase Promise que retorna a traves de la funcion promise.
 */
+
+// Otro ejemplo
+const doTask = (iterations) => {
+  return new Promise((resolve, reject) => {
+    const numbers = [];
+
+    for (let i = 0; i < iterations; i++) {
+      const number = 1 + Math.floor(Math.random() * 6);
+      numbers.push(number);
+      if (number === 6) {
+        reject({
+          error: true,
+          message: "Se ha sacado un 6",
+        });
+      }
+    }
+
+    resolve({
+      error: false,
+      value: numbers,
+    });
+  });
+};
+
+doTask(5)
+  .then((result) => console.log("Tiradas correctas: ", result.value))
+  .catch((err) => console.error("Ha ocurrido algo: ", err.message));
