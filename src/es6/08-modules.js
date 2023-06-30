678c22e3c6bf5bf8847080c303eeea3f015ffcd9
renpereiradx <renedx92@gmail.com>
2023-06-29 22:29:41 -0400

    Ejemplo para la clase Module. Nuevas clases: generator y sets.

--- src/es6/08-modules.js
+++ src/es6/08-modules.js
@@ -1,3 +1,13 @@
 import saludar from "./module.js";
-
 saludar();
+
+// Reto Platzi
+import { getData } from "./module.js";
+
+function solution() {
+  return getData().then((response) => response);
+}
+
+solution().then((response) => {
+  console.log(response);
+});
--- src/es6/module.js
+++ src/es6/module.js
@@ -1,5 +1,24 @@
 const saludar = () => {
-    console.log("Hola!");
-}
-
+  console.log("Hola!");
+};
 export default saludar;
+
+// Reto Platzi
+export async function getData() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
+        {
+          movie: "El despertar de los michis",
+          year: 2021,
+          protagonist: "Mr. Michi",
+        },
+        {
+          movie: "101 Michis",
+          year: 2019,
+          protagonist: "Tommy Michiguire",
+        },
+      ]);
+    }, 1000);
+  });
+}
